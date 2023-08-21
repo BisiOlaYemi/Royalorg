7e005de4162927d6764001623f557bbf014ad4b0
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-21 12:44:03 -0700

    Updated Link cp

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -9,32 +9,32 @@ const Footer = () => {
       <div className="relative container mx-auto py-10">
         <div className="flex justify-center">
           <div className="flex items-center space-x-6">
-            <Link href="#" className="hover:text-gray-400">
+            <Link to="/" className="hover:text-gray-400">
               Home
             </Link>
             <span className="text-gray-400">|</span>
-            <Link href="#" className="hover:text-gray-400">
+            <Link to="#" className="hover:text-gray-400">
               About Us
             </Link>
             <span className="text-gray-400">|</span>
-            <Link href="#" className="hover:text-gray-400">
+            <Link to="#" className="hover:text-gray-400">
               Services
             </Link>
             <span className="text-gray-400">|</span>
-            <Link href="#" className="hover:text-gray-400">
+            <Link to="#" className="hover:text-gray-400">
               Contact Us
             </Link>
           </div>
         </div>
         <hr className="border-t border-gray-400 my-4" />
         <div className="flex justify-center items-center space-x-4 mt-12 mb-12 ">
-          <Link href="#" className="text-white hover:text-gray-400">
+          <Link to="#" className="text-white hover:text-gray-400">
             <FaFacebook size={24} />
           </Link>
-          <Link href="#" className="text-white hover:text-gray-400">
+          <Link to="#" className="text-white hover:text-gray-400">
             <FaTwitter size={24} />
           </Link>
-          <Link href="#" className="text-white hover:text-gray-400">
+          <Link to="#" className="text-white hover:text-gray-400">
             <FaLinkedin size={24} />
           </Link>
         </div>
