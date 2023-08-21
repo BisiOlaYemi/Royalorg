9a0ccc5a089dc37c020db243b2357111f63c76c3
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-21 12:42:40 -0700

    Changed anchor to Link

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
@@ -8,34 +9,34 @@ const Footer = () => {
       <div className="relative container mx-auto py-10">
         <div className="flex justify-center">
           <div className="flex items-center space-x-6">
-            <a href="#" className="hover:text-gray-400">
+            <Link href="#" className="hover:text-gray-400">
               Home
-            </a>
+            </Link>
             <span className="text-gray-400">|</span>
-            <a href="#" className="hover:text-gray-400">
+            <Link href="#" className="hover:text-gray-400">
               About Us
-            </a>
+            </Link>
             <span className="text-gray-400">|</span>
-            <a href="#" className="hover:text-gray-400">
+            <Link href="#" className="hover:text-gray-400">
               Services
-            </a>
+            </Link>
             <span className="text-gray-400">|</span>
-            <a href="#" className="hover:text-gray-400">
+            <Link href="#" className="hover:text-gray-400">
               Contact Us
-            </a>
+            </Link>
           </div>
         </div>
         <hr className="border-t border-gray-400 my-4" />
         <div className="flex justify-center items-center space-x-4 mt-12 mb-12 ">
-          <a href="#" className="text-white hover:text-gray-400">
+          <Link href="#" className="text-white hover:text-gray-400">
             <FaFacebook size={24} />
-          </a>
-          <a href="#" className="text-white hover:text-gray-400">
+          </Link>
+          <Link href="#" className="text-white hover:text-gray-400">
             <FaTwitter size={24} />
-          </a>
-          <a href="#" className="text-white hover:text-gray-400">
+          </Link>
+          <Link href="#" className="text-white hover:text-gray-400">
             <FaLinkedin size={24} />
-          </a>
+          </Link>
         </div>
       </div>
       <div className="absolute bottom-0 left-0 right-0 p-4 text-center text-gray-400">
