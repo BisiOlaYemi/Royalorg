d61e053a71701cb17a98bba98023cc071d1e5b4e
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-22 06:41:06 -0700

    Added layout to about us

--- src/components/Pages/AboutUs.jsx
+++ src/components/Pages/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import MainLayout from '../Layouts/MainLayouts'
 
 const AboutUs = () => {
   return (
+    <MainLayout>
     <div>
       <h1>Hello About</h1>
     </div>
+    </MainLayout>
   )
 }
 
