fb349a361e168718361e7970ed6cc560980b1bcf
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-22 06:41:48 -0700

    Added main layout to services

--- src/components/Pages/Services.jsx
+++ src/components/Pages/Services.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import MainLayout from '../Layouts/MainLayouts'
 
 const Services = () => {
   return (
+    <MainLayout>
     <div>
       <h1 className='text-black'>Hello Service</h1>
     </div>
+    </MainLayout>
   )
 }
 
