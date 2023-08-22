ccc157f03fc0c1a3bc1c3365e2bc0667b2fec1e4
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-22 06:42:47 -0700

    Added Main Layout to Contact us

--- src/components/Pages/ContactUs.jsx
+++ src/components/Pages/ContactUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import MainLayout from '../Layouts/MainLayouts'
 
 const ContactUs = () => {
   return (
+    <MainLayout>
     <div>
       <h1>Hello Contact</h1>
     </div>
+    </MainLayout>
   )
 }
 
