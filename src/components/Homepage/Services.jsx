168b98dc96169c8282000deae419729de47d11b6
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-25 04:56:47 -0700

    Updating the Service Page

--- src/components/Homepage/Services.jsx
+++ src/components/Homepage/Services.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { RiTruckFill, RiCalendarEventFill } from 'react-icons/ri';
+import { BsAirplaneFill } from 'react-icons/bs';
+import { FaSatelliteDish } from 'react-icons/fa';
 
 const Services = () => {
   return (
@@ -12,6 +15,7 @@ const Services = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="bg-gray-800 rounded-lg shadow-md p-6 hover:bg-gray-50 transform hover:scale-105 transition-all">
             <div className="flex flex-col items-center justify-center h-full">
+                <RiTruckFill className="text-3xl mb-2 text-sky-700" />
               <h3 className="text-xl text-sky-700 font-semibold mb-4">Logistics </h3>
               <ul className="text-green-600 list-disc pl-4">
                 <li>Part Loads & Full Loads</li>
@@ -25,6 +29,7 @@ const Services = () => {
 
           <div className="bg-gray-800 rounded-lg shadow-md p-6 hover:bg-gray-50 transform hover:scale-105 transition-all">
             <div className="flex flex-col items-center justify-center h-full">
+                <BsAirplaneFill className="text-3xl mb-2 text-sky-700" />
               <h3 className="text-xl text-sky-700 font-semibold mb-4">Haulage </h3>
               <ul className="text-green-600 list-disc pl-4">
                 <li>General Air Freight Products</li>
@@ -38,6 +43,7 @@ const Services = () => {
 
           <div className="bg-gray-800 rounded-lg shadow-md p-6 hover:bg-gray-50 transform hover:scale-105 transition-all">
             <div className="flex flex-col items-center justify-center h-full">
+                <FaSatelliteDish className="text-3xl mb-2 text-sky-700" />
               <h3 className="text-xl text-sky-700 font-semibold mb-4">Telecom</h3>
               <ul className="text-green-600 list-disc pl-4">
                 <li>Voice services</li>
@@ -49,6 +55,7 @@ const Services = () => {
           </div>
           <div className="bg-gray-800 rounded-lg shadow-md p-6 hover:bg-gray-50 transform hover:scale-105 transition-all">
             <div className="flex flex-col items-center justify-center h-full">
+                <RiCalendarEventFill className="text-3xl mb-2 text-sky-700" />
               <h3 className="text-xl text-sky-700 font-semibold mb-4">Event Management</h3>
               <ul className="text-green-600 list-disc pl-4">
                 <li>On-site management</li>
