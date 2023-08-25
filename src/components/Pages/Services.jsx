84a6b422c82916e21843f9bd9ba3338d870583e0
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-25 04:22:52 -0700

    Updating  change on Service page

--- src/components/Pages/Services.jsx
+++ src/components/Pages/Services.jsx
@@ -12,7 +12,7 @@ const Services = () => {
         <p className="text-[2.5rem] text-center md:text-left lg:text-[3.5rem] font-bold tracking-wide text-headingColor md:w-[85%] md:leading-tight">
           <span className="bg-gradient-to-r from-purple-500 to-amber-500 text-transparent bg-clip-text text-[2rem] lg:text-[4rem]">Let's Work</span>  Together
         </p>
-          <p className=' mt-2'>
+          <p className='mt-2'>
           Efficient transportation services for your goods and materials, latest telecommunications solutions
           to keep your business connected. We offer a wide range of services, including voice, data,
           and internet, We can help you plan, organize, and execute your events, from small corporate
