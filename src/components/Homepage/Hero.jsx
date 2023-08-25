7c55f2bb2ed817e3e22ab12a44dae54e0ab37224
BisiOlaYemi <ogunrinde_olayemi@yahoo.com>
2023-08-25 05:43:42 -0700

    Added framer motion to component

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -5,18 +5,27 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import heritage1 from '../../assets/heritage1.png';
 import heritage2 from '../../assets/heritage2.png';
 import heritage3 from '../../assets/heritage3.png';
+import { motion } from 'framer-motion';
+import 'tailwindcss/tailwind.css';
 
 
 const Hero = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full sm:m12 mt-24" id="home">
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6 p-8">
-        <p className="text-[2.5rem] text-center md:text-left lg:text-[3.5rem] font-bold tracking-wide text-headingColor md:w-[85%] md:leading-tight">
-          <span className="bg-gradient-to-r from-purple-500 to-amber-500 text-transparent bg-clip-text text-[3rem] lg:text-[4rem]">Streamline</span>  Your Logistics
-        </p>
-        <p className="text-center md:text-justify text-black text-lg md:w-[85%] flex flex-wrap gap-1 font-medium">
-        Efficiently manage your haulage, telecom services, and events with Royal heritage Intl Enterprise's comprehensive solutions.
-        </p>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.9 }}
+          className=" p-4"
+          >
+          <p className="text-[2.5rem] text-center md:text-left lg:text-[3.5rem] font-bold tracking-wide text-headingColor md:w-[85%] md:leading-tight">
+            <span className="bg-gradient-to-r from-purple-500 to-amber-500 text-transparent bg-clip-text text-[3rem] lg:text-[4rem]">Streamline</span>  Your Logistics
+          </p>
+          <p className="text-center md:text-justify text-black text-lg md:w-[85%] flex flex-wrap gap-1 font-medium">
+            Efficiently manage your haulage, telecom services, and events with Royal heritage Intl Enterprise's comprehensive solutions.
+          </p>
+        </motion.div>
         <div className="text-center md:w-[85%] flex flex-wrap gap-1 md:gap-3">
           <Link to="">
             <button className="bg-blue-950 text-white md:px-7 px-4 py-2 text-lg rounded-full hover:bg-gray-800 hover:scale-105 transition-all duration-300">Get Started</button>
--- src/components/Homepage/Royal.jsx
+++ src/components/Homepage/Royal.jsx
@@ -1,27 +1,58 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import Heri from '../../assets/Heri.png';
 import { Link } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 
 const Royal = () => {
+  const [containerRef, inView] = useInView({
+    triggerOnce: true, // Only trigger the animation once
+  });
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+  };
+
+  const imageVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { delay: 0.5, duration: 0.8 } },
+  };
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-x-0 px-4 md:px-24 mt-16'>
+      <motion.div
+        className="flex justify-center items-center"
+        variants={imageVariants}
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"} // Animate only when inView is true
+        ref={containerRef} // Attach the ref to the motion div
+      >
+        <img src={Heri} alt='heritage' className="max-w-full h-auto w-64 md:w-full rounded-lg hover:scale-105 transition-all duration-300" />
+      </motion.div>
 
-        <div className="flex justify-center items-center">
-            <img src={Heri} alt='heritage' className="max-w-full h-auto w-64 md:w-full rounded-lg" />
-        </div>
-
-      <div className="md:pr-8">
+      <motion.div
+        className="md:pr-8"
+        variants={containerVariants}
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"} 
+        ref={containerRef} 
+      >
         <h2 className="text-center bg-gradient-to-r from-rose-800 to-blue-800 text-transparent font-bold bg-clip-text text-[3rem] lg:text-[4rem] mb-2">Royal Heritage International Enterprises</h2>
-        <p className="text-center md:text-left font-[Open Sans] font-medium"> is one of the leading provider of logistics, haulage, telecom services in Nigeria, including event management solutions - we specialize in delivering top-notch services to our clients, helping them streamline their operations and achieve their business goals. With our extensive industry experience and a dedicated team of professionals, we pride ourselves on delivering exceptional results and exceeding customer expectations. Whether you need efficient transportation solutions, reliable telecom services, or expert event management, Royal is your trusted partner for all your business needs. At the heart of our operational philosophy lies an unwavering dedication to supporting diverse industries. We comprehend the intricacies of various sectors and tailor our services to their unique demands. From the dynamic realm of technology to the intricate tapestry of fashion, from the essentials of food to the complexities of machinery.</p>
+        <p className="text-center md:text-left font-[Open Sans] font-medium"> 
+          is one of the leading provider of logistics, haulage, telecom services in Nigeria, including event management solutions - we specialize in delivering
+          top-notch services to our clients, helping them streamline their operations and achieve their business goals. With our extensive industry experience and
+          a dedicated team of professionals, we pride ourselves on delivering exceptional results and exceeding customer expectations. Whether you need efficient
+          transportation solutions, reliable telecom services, or expert event management, Royal is your trusted partner for all your business needs. At the heart of our operational philosophy lies an
+          unwavering dedication to supporting diverse industries. We comprehend the intricacies of various sectors and tailor our services to their unique demands. From the dynamic realm of technology to
+          the intricate tapestry of fashion, from the essentials of food to the complexities of machinery.
+        </p>
         <div className='mt-12'>
-        <Link to="">
+          <Link to="">
             <button className="bg-blue-950 text-white md:px-7 px-4 py-2 text-lg rounded-full hover:bg-gray-800 hover:scale-105 transition-all duration-300">Learn More</button>
           </Link>
         </div>
-      </div>
-
-      
-
+      </motion.div>
     </div>
   );
 };
