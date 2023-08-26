253cd276635f7908af6707c33d4244a5bd60f825
Olayemi <ogunrinde_olayemi@yahoo.com>
2023-08-26 04:33:55 -0700

    Updating components

--- src/components/Pages/ContactUs.jsx
+++ src/components/Pages/ContactUs.jsx
@@ -80,7 +80,7 @@ const ContactUs = () => {
             </div>
             <div className="flex items-center justify-center">
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                className="bg-teal-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
               >
                 Submit
